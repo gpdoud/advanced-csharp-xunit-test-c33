7a4f98b8b9f75af181d94d385cc62f91451ca8fe
Greg Doud <gpdoud@gmail.com>
2021-10-20 16:40:50 -0400

    Final commit

--- TestMathLibrary/TestMathLib.cs
+++ TestMathLibrary/TestMathLib.cs
@@ -7,6 +7,15 @@ namespace TestMathLibrary {
 	public class TestMathLib {
 
 		[Theory]
+		[InlineData(2, 1, 2)]
+		[InlineData(2, -1, -2)]
+		[InlineData(-2, -1, 2)]
+		[InlineData(0, 0, -2)]
+		[InlineData(1, -1, 0)]
+		public void TestMult(int ans, int a, int b) {
+			Assert.Equal(ans, MathLibrary.MathLib.Mult(a, b));
+		}
+		[Theory]
 		[InlineData(3, 1, 2)]
 		[InlineData(-3, -1, -2)]
 		public void TestAdd(int ans, int a, int b) {
